2a44ef3f74925ae73aa51d56299056752003bd5d
Alberto Finardi <finardialberto@gmail.com>
2022-01-30 16:39:08 +0100

    modules

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
-bearer_token = "AAAAAAAAAAAAAAAAAAAAALO0YQEAAAAAtwzv%2F%2BE1%2F5h5paDHvjllHzKMPdU%3Dc4QTiiKZd9EwX3xqqFaYtT6lj20y2B2kV0dE2QNwbMwmz1Scir"
+bearer_token = ""
 max_tweets = 500
 max_added_or_fetched = 'fetched'
 query = '(from:twitterdev -is:retweet) OR #twitterdev'
 
+
+backup_folder = 'backup'
 export_csv = 'data.csv'
+export_csv_backup = 'data_backup.csv'
 export_json = 'data.json'
+export_json_backup = 'data_json.json'
